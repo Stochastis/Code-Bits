68882a808d143ef255ecc926214fc1d577aa292c
Stochastis <59930635+Stochastis@users.noreply.github.com>
2022-10-03 15:34:12 -0500

    Added MatrixChainOrder

---
+++ MatrixChainOrder.py
@@ -0,0 +1,45 @@
+from asyncio.windows_events import NULL
+
+
+def MatrixChainOrder(p):
+    # Number of matrices
+    n = len(p) - 1
+
+    # Width and height equals number of matrices
+    m = [[NULL for i in range(1, n + 1)] for j in range(1, n + 1)]
+
+    s = [[NULL for i in range(1, n)] for j in range(2, n + 1)]
+
+    # For every matrix
+    for i in range(n):
+        # Set the diagonal to zeroes
+        m[i][i] = 0
+
+    # Similar to the second dimension in the 's' array.
+    # Not sure if there's a relation there or not yet.
+    # "l is the chain length"
+    for l in range(2, n + 1):
+        for i in range(1, n - l + 2):
+            j = i + l - 1
+            m[i - 1][j - 1] = float("inf")
+            for k in range(i, j):
+                q = m[i - 1][k - 1] + m[k][j - 1] + p[i - 1] * p[k] * p[j]
+                if q < m[i - 1][j - 1]:
+                    m[i - 1][j - 1] = q
+                    s[i - 1][j - 2] = k
+    return m, s
+
+
+def PrintOptimalParens(s, i, j):
+    if i == j:
+        return "A%d" % (i)
+    else:
+        return "(%s%s)" % (
+            PrintOptimalParens(s, i, s[i - 1][j - 2]),
+            PrintOptimalParens(s, s[i - 1][j - 2] + 1, j),
+        )
+
+
+m, s = MatrixChainOrder([30, 35, 15, 5, 10, 20, 25])
+
+print(PrintOptimalParens(s, 1, 6))
